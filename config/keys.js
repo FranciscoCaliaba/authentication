5a29a85a7342dd93bfd5b75ae6c32ad6ccd1b4b3
FranciscoCaliaba <fcaliaba1997@gmail.com>
2021-03-03 03:14:31 -0300

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js figure out what set of credentials to return
+if (process.env.NODE_ENV === "production") {
+  // we are in production - return the productions keys
+  module.exports = require('./prod')
+} else {
+  // we are in development - return the developments keys
+  module.exports = require("./dev");
+}
